e1200c7a2fb8b14888fc309f912c3099bbd1bc85
Pranjal <pranjalpundir37@gmail.com>
2026-07-10 18:05:38 +0530

    registration pg usestate

--- client/src/Pages/Registration.jsx
+++ client/src/Pages/Registration.jsx
@@ -9,22 +9,37 @@ const Registration = () => {
 
     const navigate = useNavigate();
     const [bgimg, setbgimg] = useState(bgcar);
-    const [message, submitAction, isPending] = useActionState(handlesubmit, null);
+    const [formData, setFormData] = useState({
+        name:"",
+        lastname:"",
+        email:"",
+        phone:"",
+        country:"",
+        state:"",
+        city:"",
+    });
+    const [isPending, setIsPending] = useState(false);
     const [showSuccess, setshowSuccess]=useState(false);
 
-    async function handlesubmit(prevState, formData) {
+    const handleChange = (e)=>{
+        const {name, value} = e.target;
 
-        const name = formData.get("name");
-        const lastname = formData.get("lastname");
-        const email = formData.get("email");
-        const phone = formData.get("phone");
-        const country = formData.get("country");
-        const state = formData.get("state");
-        const city = formData.get("city");
+        setFormData((prevData)=>({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const handlesubmit = async (e) =>{
+        e.preventDefault();
+
+        setIsPending(true);
 
         await new Promise ((resolve)=>{
             setTimeout(resolve,2000);
         });
+        
+        setIsPending(false);
         setshowSuccess(true);
         
 
@@ -60,34 +75,34 @@ const Registration = () => {
             <div className="max-w-2xl mx-4 sm:mx-8 md:mx-auto mt-4 sm:mt-20 border p-6 sm:pb-10 sm:mb-10 rounded-lg bg-white/18 text-white">
                 <h1 className="text-lg sm:text-2xl lg:text-4xl font-bold ml-[80px] sm:ml-[150px] mb-[30px] pt-1">Registration Form</h1><br />
                 <div className="sm:ml-20">
-                    <form action={submitAction}>
+                    <form onSubmit={handlesubmit}>
                         <div className="flex flex-col sm:flex-row sm:items-center mb-[8px]">
                             <label htmlFor="name" className="sm:w-32 text-sm sm:text-base lg:text-xl mb-1 sm:mb-0">Name : </label>
-                            <input type="text" id="name" name="name" className="w-full sm:w-72 border border-gray-400 rounded-sm pl-2" placeholder="Enter your name" required />
+                            <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} className="w-full sm:w-72 border border-gray-400 rounded-sm pl-2" placeholder="Enter your name" required />
                         </div>
                         <div className="flex flex-col sm:flex-row sm:items-center mb-[8px]">
                             <label htmlFor="lastname" className="sm:w-32 text-sm sm:text-base lg:text-xl mb-1 sm:mb-0">Last Name : </label>
-                            <input type="text" id="lastname" name="lastname" className="w-full sm:w-72 border border-gray-400 rounded-sm pl-2" placeholder="Enter your last name" required />
+                            <input type="text" id="lastname" name="lastname"  value={formData.lastname} onChange={handleChange} className="w-full sm:w-72 border border-gray-400 rounded-sm pl-2" placeholder="Enter your last name" required />
                         </div>
                         <div className="flex flex-col sm:flex-row sm:items-center mb-[8px]">
                             <label htmlFor="email" className="sm:w-32 text-sm sm:text-base lg:text-xl mb-1 sm:mb-0">Email : </label>
-                            <input type="email" id="email" name="email" className="w-full sm:w-72 border border-gray-400 rounded-sm pl-2" placeholder="Enter your email" required />
+                            <input type="email" id="email" name="email"  value={formData.email} onChange={handleChange} className="w-full sm:w-72 border border-gray-400 rounded-sm pl-2" placeholder="Enter your email" required />
                         </div>
                         <div className="flex flex-col sm:flex-row sm:items-center mb-[8px]">
                             <label htmlFor="phone" className="sm:w-32 text-sm sm:text-base lg:text-xl mb-1 sm:mb-0">Phone no : </label>
-                            <input type="tel" id="phone" name="phone" className="w-full sm:w-72 border border-gray-400 rounded-sm pl-2" placeholder="Enter your phone no." maxLength={10} pattern="[6-9][0-9]{9}" required />
+                            <input type="tel" id="phone" name="phone"  value={formData.phone} onChange={handleChange} className="w-full sm:w-72 border border-gray-400 rounded-sm pl-2" placeholder="Enter your phone no." maxLength={10} pattern="[6-9][0-9]{9}" required />
                         </div>
                         <div className="flex flex-col sm:flex-row sm:items-center mb-[8px]">
                             <label htmlFor="country" className="sm:w-32 text-sm sm:text-base lg:text-xl mb-1 sm:mb-0">Country : </label>
-                            <input type="text" id="country" name="country" className="w-full sm:w-72 border border-gray-400 rounded-sm pl-2" placeholder="Enter your country" required />
+                            <input type="text" id="country" name="country"  value={formData.country} onChange={handleChange} className="w-full sm:w-72 border border-gray-400 rounded-sm pl-2" placeholder="Enter your country" required />
                         </div>
                         <div className="flex flex-col sm:flex-row sm:items-center mb-[8px]">
                             <label htmlFor="state" className="sm:w-32 text-sm sm:text-base lg:text-xl mb-1 sm:mb-0">State : </label>
-                            <input type="text" id="state" name="state" className="w-full sm:w-72 border border-gray-400 rounded-sm pl-2" placeholder="Enter your state" required />
+                            <input type="text" id="state" name="state"  value={formData.state} onChange={handleChange} className="w-full sm:w-72 border border-gray-400 rounded-sm pl-2" placeholder="Enter your state" required />
                         </div>
                         <div className="flex flex-col sm:flex-row sm:items-center mb-[8px]">
                             <label htmlFor="city" className="sm:w-32 text-sm sm:text-base lg:text-xl mb-1 sm:mb-0">City : </label>
-                            <input type="text" id="city" name="city" className="w-full sm:w-72 border border-gray-400 rounded-sm pl-2" placeholder="Enter your city" required />
+                            <input type="text" id="city" name="city"  value={formData.city} onChange={handleChange} className="w-full sm:w-72 border border-gray-400 rounded-sm pl-2" placeholder="Enter your city" required />
                         </div>
                         <div className="flex justify-center mt-10">
                             <button type="submit" disabled={isPending} className="bg-orange-600 text-white w-24 sm:w-32 lg:w-40 mb-2 rounded-sm border border-white-2 font-semibold hover:bg-orange-700 transition all duration-300">{isPending ?"submitting...": "Register"}</button>
