1cd6996508764903ecd1fc07a8dcbe24b9f0f670
Pranjal <pranjalpundir37@gmail.com>
2026-07-13 12:42:46 +0530

    backend connected with mongodb

--- server/server.js
+++ server/server.js
@@ -1,23 +1,30 @@
-import connectDB from "./config/db.js";
 import express from "express";
-import cors from "cors";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
+import dns from "dns";
+
+dns.setServers(["1.1.1.1","8.8.8.8"]);
 
 dotenv.config();
-connectDB();
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res) => {
-    res.json({
-        message:"car rental backend running"
-    });
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+    console.log("MongoDB Connected Successfully");
+})
+.catch((error) => {
+    console.log(error);
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`);
+
+app.get("/", (req, res) => {
+    res.send("Server is running");
+});
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
 });
